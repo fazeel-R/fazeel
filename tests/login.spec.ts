994fc4bbf6be41aec590cddce2da03113dc9c282
fazeel-R <mfazeel307@gmail.com>
2025-12-04 21:33:50 +0530

    implemented json file creds reading

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -10,7 +10,7 @@ test("Login to the buyer", async({page})=>{
     //await buyerLogin.loadUrl('https://pxnrfort-test.powerofn.in/')
     await buyerLogin.loadUrl(creds.buyerUrl)
     console.log(page.url())
-    await buyerLogin.loginDetail('fazeel@ncompass.inc', 'Admin123!')
+    await buyerLogin.loginDetail(creds.buyerCreds.userName, creds.buyerCreds.password)
     await buyerLogin.login()
 
 })
