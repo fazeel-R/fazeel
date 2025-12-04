77463eaa5c9e3a5163d06da2fe22a765bed93d25
fazeel-R <mfazeel307@gmail.com>
2025-12-04 21:28:02 +0530

    Implementing reading creds from json

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,15 @@
 import test from '@playwright/test'
 
+const creds = require('../creds.json')
+
 import { BuyerLogin } from '../pages/buyerLogin'
 
 test("Login to the buyer", async({page})=>{
 
     let buyerLogin = new BuyerLogin(page)
-    await buyerLogin.loadUrl('https://pxnrfort-test.powerofn.in/')
+    //await buyerLogin.loadUrl('https://pxnrfort-test.powerofn.in/')
+    await buyerLogin.loadUrl(creds.buyerUrl)
+    console.log(page.url())
     await buyerLogin.loginDetail('fazeel@ncompass.inc', 'Admin123!')
     await buyerLogin.login()
 
